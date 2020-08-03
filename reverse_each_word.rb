d93fb5f83a7eafef2706c6e8e4ba80d7a143044b
Brian Donnoe <dunow33@hotmail.com>
2020-08-03 07:26:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(sentence)
   
   stringSentence = []
   
-  stringArray.collect_with_index do |word, index|
+  stringArray.each_with_index do |word, index|
     if (stringArray.length - 1) == index
       stringSentence.push(word.reverse)
     elsif
