dc60bd28f654c158db7a598b06f5bcd5fb6dae77
Brian Donnoe <dunow33@hotmail.com>
2020-08-03 07:37:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,16 @@
 def reverse_each_word(sentence)
   stringArray = sentence.split
-  #size = stringArray.length - 1
+  counter = 0
   
   stringSentence = []
   
   stringArray.each do |word|
-    #if (stringArray.length - 1) == stringArray[size]
+    if (stringArray.length - 1) == counter
       stringSentence.push(word.reverse)
-    #elsif
-     # stringSentence.push(word.reverse + " ")
-    #end
+    elsif
+      stringSentence.push(word.reverse + " ")
+      counter += 1
+    end
   end
   
   return stringSentence.join
